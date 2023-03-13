fd44a20cdd5bc62fc850673d781e644050720d40
Hernandez Hernandez Areli <al221910333@gmail.com>
2023-03-12 22:30:26 -0600

    Diseños home, login, resgister

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="container-fluid w-100 vh-100 row align-items-center m-0 bg-sm-login">
+        <div class="row h-75">
+            <div class="col-sm-12 col-md-10 col-lg-8 align-items-center m-auto shadow-lg bg-light">
+                <div class="card-header btn-amarillo text-blanco w-100 fw-bold p-2 mb-2 text-center">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end fw-bold text-verde">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end fw-bold text-verde" >{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end fw-bold text-verde">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end fw-bold text-verde">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -63,10 +63,13 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-verde text-blanco text-center fw-bold">
                                     {{ __('Register') }}
                                 </button>
                             </div>
+                            <a class="btn btn-link text-verde" href="{{ route('login') }}">
+                                {{ __('Ya tienes una cuenta') }}
+                            </a>
                         </div>
                     </form>
                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,77 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <aside class="controls-clients">
+        <div class="my-3">
+            <button type="button" class="btn btn-verde" data-bs-toggle="modal" data-bs-target="#createClient">
+                <i class="bi bi-person-plus-fill text-blanco"></i>
+            </button>
+            <span class="fw-bold overlay">Agregar Nuevo Registro</span>
+        </div>
+    </aside>
+    <div class="row h-25 p-3">
+        <div class="col">
+            <h1 class="text-center mt-3 text-verde fw-bold">Directorio de Registros</h1>
+            <br><br>
+            <div class="row ps-5 pe-5 justify-content-center text-center text-white fw-bold">
+                <div class="col-lg-1 mx-5 btn-amarillo text-blanco">
+                    <p class="m-0 ">
+                        <span class="d-block" >Registros</span>
+                        Total
+                    </p>
+                </div>
+                
+                <div class="col-lg-1 mx-5">
+                    <a href="#" class="btn btn-verde ms-1 text-blanco" target="_blank">Exportar <strong>Semestre A</strong></a>
+                </div>
+                <div class="col-lg-1 mx-5">
+                    <a href="#" class="btn btn-verde ms-1 text-blanco" target="_blank">Exportar <strong>Semestre B</strong></a>
                 </div>
             </div>
+            <br>
         </div>
     </div>
-</div>
+
+    <div class="container-fluid pt-md-2 p-md-5 pb-md-0 p-sm-2">
+        <div class="row justify-content-center mb-3">
+           
+                <div class="col-lg-4 col-xl-3 col-sm-5 col-md-6 m-2 shadow p-5 text-center provider bg-light rounded">
+                    <div class="btn-delete-container">
+                        <form action="#" method="post" title="Eliminar">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger btn-sm" onclick="return confirm('Seguro que deseas eliminar')">
+                                <i class="bi bi-x-lg"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <p class=" w-auto fs-6 fw-bold fst-italic rounded-pill">
+                        nombre 
+                    </p>
+                    <p>
+                        <i class="bi bi-telephone-fill"></i>
+                        - telefono
+                    </p>
+                    <p>
+                        <i class="bi bi-geo-alt-fill"></i>
+                        - insumo
+                    </p>
+
+                    <button
+                        data-bs-target="#editClient "
+                        data-bs-toggle="modal"
+                        class="btn btn-amarillo text-blanco btn-sm d-block w-100 btn-edit"
+                        title="Editar">
+                        <i class="bi bi-pencil-square"></i>
+                        Editar
+                    </button>
+                    
+                </div>
+
+            
+                <h3 class="text-center text-verde fw-bold">No hay registros en existencia</h3>
+            
+        </div>
+    </div>
+
 @endsection
