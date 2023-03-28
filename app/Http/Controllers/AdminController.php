13c3e5b60946a8dc5eb154d861f1cc757aa2c9ea
Hernandez Hernandez Areli <al221910333@gmail.com>
2023-03-27 22:21:07 -0600

    detalle

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,4 +19,16 @@ class AdminController extends Controller{
         ->get();
         return view('home', compact('registros'));
     }
+    public function detalle(Request $request, $id){
+        $prestamos = DB::table('prestamos')
+        ->join('aulas', 'prestamos.id_aula', '=' , 'aulas.id')
+        ->join('licenciaturas', 'prestamos.id_licenciatura', '=' , 'licenciaturas.id')
+        ->join('insumos', 'prestamos.id_insumo', '=' , 'insumos.id')
+        ->join('horas', 'prestamos.id_hora', '=' , 'horas.id')
+        ->select('nom_solicitante','no_cuenta','telefono','unidad_aprendizaje','profesor','aulas.nombre as aula', 'licenciaturas.nombre as licenciatura', 'insumos.nombre as insumo', 'fecha_solicitud', 'fecha_entrega')
+        ->where('prestamos.id', '=', $id)
+        ->get();
+        return view("detalle")
+        ->with(['prestamos' => $prestamos]);
+    }
 }
